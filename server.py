21715da699aa05238de0f8c9aa3d90617ecbed56
mateogolf <rodriguezml11@mail.wlu.edu>
2017-08-04 11:38:48 -0400

    Counter Assignment

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, request,session
+app = Flask(__name__)
+app.secret_key = "countThis"
+
+
+@app.route('/')
+def index():
+    if 'counter' in session:
+        session['counter'] += 1
+    else:
+        session['counter'] = 0
+    return render_template('index.html')
+
+
+@app.route('/level1')
+def level1():
+    session['counter'] += 1
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    if 'counter' in session:
+        session['counter'] += 1
+    else:
+        session['counter'] = 0
+    return redirect('/')
+
+app.run(debug=True)
